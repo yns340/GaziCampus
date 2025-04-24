e598dc05d8c45780fbcc337a758c03ab1ca080d2
Yunus Recepoğlu <yunusrecepoglu@192.168.1.104>
2025-04-25 00:22:32 +0300

    Ankara tasarımları

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.myapplication3;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ankaraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ankaraButton = findViewById(R.id.AnkaraButton);
+
+        ankaraButton.setOnClickListener(v -> {
+            // Yeni fragment oluştur
+            Fragment ankaraFragment = new AnkaraFragment();
+
+            // Geçişi yap
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragmentContainer, ankaraFragment);
+            transaction.addToBackStack(null); // Geri tuşu ile geri dönebilmek için
+            transaction.commit();
         });
     }
 }
